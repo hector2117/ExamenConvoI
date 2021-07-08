62c979b144b28eea86bd518110e7cd484a57276b
hector2117 <70295230+hector2117@users.noreply.github.com>
2021-07-08 08:06:14 -0600

    Perdón
    Perdón

---
+++ ExamenConvoI-main/FN/FN/Form1.Designer.cs
@@ -0,0 +1,290 @@
+﻿
+namespace FlujoNeto
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnCrear = new System.Windows.Forms.Button();
+            this.txtValSalv = new System.Windows.Forms.TextBox();
+            this.txtInflacion = new System.Windows.Forms.TextBox();
+            this.txtTasa = new System.Windows.Forms.TextBox();
+            this.txtPlazo = new System.Windows.Forms.TextBox();
+            this.txtInversion = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.gbTabla = new System.Windows.Forms.GroupBox();
+            this.txtPR = new System.Windows.Forms.TextBox();
+            this.txtVPN = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.dgvTabla = new System.Windows.Forms.DataGridView();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtIngresos = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.gbTabla.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTabla)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.txtIngresos);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.btnCrear);
+            this.groupBox1.Controls.Add(this.txtValSalv);
+            this.groupBox1.Controls.Add(this.txtInflacion);
+            this.groupBox1.Controls.Add(this.txtTasa);
+            this.groupBox1.Controls.Add(this.txtPlazo);
+            this.groupBox1.Controls.Add(this.txtInversion);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(168, 447);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Datos";
+            // 
+            // btnCrear
+            // 
+            this.btnCrear.Location = new System.Drawing.Point(31, 401);
+            this.btnCrear.Name = "btnCrear";
+            this.btnCrear.Size = new System.Drawing.Size(75, 23);
+            this.btnCrear.TabIndex = 10;
+            this.btnCrear.Text = "Crear";
+            this.btnCrear.UseVisualStyleBackColor = true;
+            this.btnCrear.Click += new System.EventHandler(this.btnCrear_Click);
+            // 
+            // txtValSalv
+            // 
+            this.txtValSalv.Location = new System.Drawing.Point(31, 331);
+            this.txtValSalv.Name = "txtValSalv";
+            this.txtValSalv.Size = new System.Drawing.Size(100, 20);
+            this.txtValSalv.TabIndex = 9;
+            // 
+            // txtInflacion
+            // 
+            this.txtInflacion.Location = new System.Drawing.Point(31, 271);
+            this.txtInflacion.Name = "txtInflacion";
+            this.txtInflacion.Size = new System.Drawing.Size(100, 20);
+            this.txtInflacion.TabIndex = 8;
+            // 
+            // txtTasa
+            // 
+            this.txtTasa.Location = new System.Drawing.Point(31, 223);
+            this.txtTasa.Name = "txtTasa";
+            this.txtTasa.Size = new System.Drawing.Size(100, 20);
+            this.txtTasa.TabIndex = 7;
+            // 
+            // txtPlazo
+            // 
+            this.txtPlazo.Location = new System.Drawing.Point(31, 168);
+            this.txtPlazo.Name = "txtPlazo";
+            this.txtPlazo.Size = new System.Drawing.Size(100, 20);
+            this.txtPlazo.TabIndex = 6;
+            // 
+            // txtInversion
+            // 
+            this.txtInversion.Location = new System.Drawing.Point(31, 110);
+            this.txtInversion.Name = "txtInversion";
+            this.txtInversion.Size = new System.Drawing.Size(100, 20);
+            this.txtInversion.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(28, 315);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(105, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Valor de Salvamento";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(28, 255);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Inflacion %";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(28, 207);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Tasa";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 152);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Plazo";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 84);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Inversion";
+            // 
+            // gbTabla
+            // 
+            this.gbTabla.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.gbTabla.Controls.Add(this.dgvTabla);
+            this.gbTabla.Controls.Add(this.txtPR);
+            this.gbTabla.Controls.Add(this.txtVPN);
+            this.gbTabla.Controls.Add(this.label7);
+            this.gbTabla.Controls.Add(this.label6);
+            this.gbTabla.Location = new System.Drawing.Point(198, 2);
+            this.gbTabla.Name = "gbTabla";
+            this.gbTabla.Size = new System.Drawing.Size(590, 447);
+            this.gbTabla.TabIndex = 3;
+            this.gbTabla.TabStop = false;
+            this.gbTabla.Text = "Tabla";
+            // 
+            // txtPR
+            // 
+            this.txtPR.Location = new System.Drawing.Point(328, 408);
+            this.txtPR.Name = "txtPR";
+            this.txtPR.ReadOnly = true;
+            this.txtPR.Size = new System.Drawing.Size(100, 20);
+            this.txtPR.TabIndex = 3;
+            // 
+            // txtVPN
+            // 
+            this.txtVPN.Location = new System.Drawing.Point(120, 409);
+            this.txtVPN.Name = "txtVPN";
+            this.txtVPN.ReadOnly = true;
+            this.txtVPN.Size = new System.Drawing.Size(100, 20);
+            this.txtVPN.TabIndex = 2;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(297, 411);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(25, 13);
+            this.label7.TabIndex = 1;
+            this.label7.Text = "PR:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(82, 412);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(32, 13);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "VPN:";
+            // 
+            // dgvTabla
+            // 
+            this.dgvTabla.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvTabla.Location = new System.Drawing.Point(17, 20);
+            this.dgvTabla.Name = "dgvTabla";
+            this.dgvTabla.Size = new System.Drawing.Size(553, 358);
+            this.dgvTabla.TabIndex = 4;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(31, 29);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(47, 13);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "Ingresos";
+            // 
+            // txtIngresos
+            // 
+            this.txtIngresos.Location = new System.Drawing.Point(31, 46);
+            this.txtIngresos.Name = "txtIngresos";
+            this.txtIngresos.Size = new System.Drawing.Size(100, 20);
+            this.txtIngresos.TabIndex = 12;
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.gbTabla);
+            this.Controls.Add(this.groupBox1);
+            this.MaximizeBox = false;
+            this.Name = "FrmMain";
+            this.Text = "Flujo Neto sin Financiamiento";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.gbTabla.ResumeLayout(false);
+            this.gbTabla.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTabla)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnCrear;
+        private System.Windows.Forms.TextBox txtValSalv;
+        private System.Windows.Forms.TextBox txtInflacion;
+        private System.Windows.Forms.TextBox txtTasa;
+        private System.Windows.Forms.TextBox txtPlazo;
+        private System.Windows.Forms.TextBox txtInversion;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox gbTabla;
+        private System.Windows.Forms.TextBox txtPR;
+        private System.Windows.Forms.TextBox txtVPN;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.DataGridView dgvTabla;
+        private System.Windows.Forms.TextBox txtIngresos;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ ExamenConvoI-main/FN/FN/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FlujoNeto
+{
+    public partial class FrmMain : Form
+    {
+        string[] rows = { "Inversion", "Ingreso", "Egreso", "Depreciacion",
+                        "V.S", "UAI", "IR", "UDI", "Depreciacion", "FNE" };
+        
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCrear_Click(object sender, EventArgs e)
+        {
+            //datos
+            decimal ingresos = decimal.Parse(txtIngresos.Text);
+            decimal inversion = decimal.Parse(txtInversion.Text);
+            int plazo = int.Parse(txtPlazo.Text);
+            decimal tasa = decimal.Parse(txtTasa.Text);
+            decimal inflacion = decimal.Parse(txtInflacion.Text);
+            decimal ValSalv = decimal.Parse(txtValSalv.Text);
+                        
+            //columnas
+            dgvTabla.Columns.Add("", "Años");
+            for(int i = 0; i <= plazo; i++)
+            {
+                dgvTabla.Columns.Add("", i.ToString());
+            }
+
+            //filas
+            for (int i = 0; i < rows.Length; i++)
+            {
+                dgvTabla.Rows.Add(rows[i]);
+            }
+
+            dgvTabla.Rows[0].Cells[1].Value = inversion;
+            for(int i = 2; i < dgvTabla.ColumnCount; i++)
+            {
+                if(i == 2)
+                {
+                    dgvTabla.Rows[1].Cells[i].Value = ingresos;
+                }
+                else
+                {
+                    ingresos = ingresos + (ingresos * inflacion);
+                    dgvTabla.Rows[1].Cells[i].Value = ingresos;
+                }
+                
+            }
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+        
+    }
+}
